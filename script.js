062e72883cddd539a81c0411ba59ac1a0fc0c5c2
Hafsi Ahmed <crasypo@gmail.com>
2022-05-12 09:13:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Développeur", "Web", "Full Stack", "Tunisien","Aime coder"],
+        strings: ["Développeur", "Web", "Full Stack", "Tunisien","Aime le coding"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Développeur", "Web", "Full Stack", "Tunisien","Aime coder"],
+        strings: ["Développeur", "Web", "Full Stack", "Tunisien","Aime le coding"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -75,42 +75,7 @@ $(document).ready(function(){
     });
 });
 
-
-
-
-/*
-
-    var btn = document.getElementById('btn');
-    btn.addEventListener('click',function(e){
-        e.preventDefault();
-
-        const inputs = document.querySelectorAll('#name, #email, #sub, #message');
-        var name = document.getElementById('name').value;
-        var email = document.getElementById('email').value;
-        var sub = document.getElementById('sub').value;
-        var message = document.getElementById('message').value;
-        var body = 'name: ' +name+ '<br/> email: ' + email + '<br/> subject: '  + sub + '<br/> message: '  + message;
-        Email.send({
-    Host : "smtp.gmail.com",
-    Username : "ahmedohafsi@gmail.com",
-    Password : "saezogavdharzdud",
-    To : 'ahmedohafsi@gmail.com',
-    From : email,
-    Subject : sub,
-    Body : body
-    
-}).then(
-    message => alert('Email Bien Reçu ! Bonne journée')
-
-);
-inputs.forEach(input => {
-    input.value = '';
-  });
-});
-
- */
-    var btn = document.getElementById('btn');
-
+        var btn = document.getElementById('btn');
         btn.addEventListener('click',function(e){
             e.preventDefault();
 
@@ -120,25 +85,66 @@ inputs.forEach(input => {
             var sub = document.getElementById('sub').value;
             var message = document.getElementById('message').value;
             var body = 'name: ' +name+ '<br/> email: ' +email+ '<br/> subject: '  +sub+ '<br/> message: '  + message;
-
-		
-			Email.send({
-			        Host : "smtp.gmail.com",
+            Email.send({
+        Host : "smtp.gmail.com",
         Username : "ahmedohafsi@gmail.com",
-        Password : "hqyxgwrpxewzhawu",
+        Password : "qbhvtudrwwnoeupf",
         To : 'ahmedohafsi@gmail.com',
         From : email,
         Subject : sub,
         Body : body
-})
-			.then(function(message){
-				alert("mail sent successfully")
-			});
-          inputs.forEach(input => {
+        
+    }).then(
+        message => alert('Email Bien Reçu ! Bonne journée')
+
+    );
+    inputs.forEach(input => {
         input.value = '';
     });
-   
-		}
-});
+    });
 
+  
+/*
+// clear all text fields form 
+    const form = document.querySelector('#myF');
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const sub = document.querySelector('#sub');
+    const msg = document.querySelector('#message');
 
+    const checkUsername = () => {
+
+        let valid = false;
+    
+        const min = 3,
+            max = 25;
+    
+        const username = name.value.trim();
+    
+        if (!isRequired(username)) {
+            showError(name, 'Username cannot be blank.');
+        } else if (!isBetween(username.length, min, max)) {
+            showError(name, `Username must be between ${min} and ${max} characters.`)
+        } else {
+            showSuccess(name);
+            valid = true;
+        }
+        return valid;
+    };
+    
+    
+    const checkEmail = () => {
+        let valid = false;
+        const email = email.value.trim();
+        if (!isRequired(email)) {
+            showError(email, 'Email cannot be blank.');
+        } else if (!isEmailValid(email)) {
+            showError(email, 'Email is not valid.')
+        } else {
+            showSuccess(email);
+            valid = true;
+        }
+        return valid;
+    };
+    
+*/
