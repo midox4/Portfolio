0be3529e99470e481e8eb136b3fcf5180e3a48c2
Hafsi Ahmed <crasypo@gmail.com>
2022-06-21 15:15:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Développeur", "Web", "Full Stack", "Tunisien","Aime le coding"],
+        strings: ["Développeur", "Web Full", "Stack JS", "Tunisien","Passionné par le coding"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Développeur", "Web", "Full Stack", "Tunisien","Aime le coding"],
+        strings: ["Développeur", "Web Full", "Stack JS", "Tunisien","Passionné par le coding"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
